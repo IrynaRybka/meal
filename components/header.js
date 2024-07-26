0a209bdb693350e409d06f88bbd77a982fb8196d
IrynaRybka <irarybka@gmail.com>
2024-07-26 11:05:20 +0300

    add styles module.css

--- components/header.js
+++ components/header.js
@@ -1,3 +1,26 @@
+import NewsLogo from '@/assets/logo.png';
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from "./header.module.css"
 export default function Header() {
-    return <><header>Header</header></>
+  return (
+    <>
+      <header className={styles.header}>
+        <Link className={styles.logo} href='/'>
+          <Image src={NewsLogo} alt='food and drinks' priority/>
+          Next Level Food 
+        </Link>
+        <nav className={styles.nav}>
+            <ul>
+                <li>
+                    <Link href="/meals">Browse Meals</Link>
+                </li>
+                <li>
+                    <Link href="/community">Food Community</Link>
+                </li>
+            </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
