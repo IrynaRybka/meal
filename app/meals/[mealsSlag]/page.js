ee936a3c501a333362a20236f64b9946ab267dfb
IrynaRybka <irarybka@gmail.com>
2024-07-30 15:59:16 +0300

    add dinamic data in mealSlug

--- app/meals/[mealsSlag]/page.js
+++
@@ -1,3 +0,0 @@
-export default function MealsDetails() {
-    return <><h2>MealsDetails</h2></>
-}
--- app/meals/error.js
+++ app/meals/error.js
@@ -3,7 +3,7 @@ export default function Error() {
   return (
     <>
       <main className='error'>
-        <h1>An error occurate!</h1>
+        <h1>Meal not found</h1>
         <p>Faild to fetch meal data. Please try again later.</p>
       </main>
     </>
