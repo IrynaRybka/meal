066d597413b571c407c3bbecdb301da6ad293730
IrynaRybka <irarybka@gmail.com>
2024-08-01 10:51:01 +0300

    add useActionState instend useFormState

---
+++ app/meals/share/error.js
@@ -0,0 +1,11 @@
+'use client';
+export default function Error() {
+  return (
+    <>
+      <main className='error'>
+        <h1>Meal not found</h1>
+        <p>Faild to create meal.</p>
+      </main>
+    </>
+  );
+}
